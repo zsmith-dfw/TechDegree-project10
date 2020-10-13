9534a29956c683eeb2815feb516b6c69284562e5
Zach Smith <rysskismith@gmail.com>
2020-10-12 19:56:27 -0500

    changed properties in courses

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Course = (props) => (
   <div className="grid-33">
-    <Link className="course--module course--link" to="/courses/">
+    <Link className="course--module course--link" to="/courses/:id">
     {/* <Link to="/courses?sort=id" /> */}
       <h4 className="course--label">Course</h4>
       <h3 className="course--title">{props.title}</h3>
